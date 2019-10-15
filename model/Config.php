bb2c2c969802d4aa50649164b6599227b6449d30
Wade Abdoulaye <papa.wade.1993@gmail.com>
2019-10-15 01:14:39 +0000

    Dossier du model ou la gestion des tables du Projet

---
+++ model/Config.php
@@ -0,0 +1,14 @@
+<?php
+#######********                               *********##########
+########******* Coding BY SON EXCELLENECE WADE **********#########
+#######*********                               *********##########
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+ class DatabaseConfig{
+
+    public static function params(){
+        return array("localhost", "gestionRV","son excellence", 1993, );
+    }
+
+}
